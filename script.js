0475353adf034bce468e527c9b894fc1d61dc917
Elena <42415402+ElenaSamanchuk@users.noreply.github.com>
2026-04-09 16:13:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const endDate = new Date("2026-04-03T23:59:59");
+const endDate = new Date("2026-04-10T23:59:59");
 function getWordForm(n, forms) {
   return forms[
     n % 10 === 1 && n % 100 !== 11
